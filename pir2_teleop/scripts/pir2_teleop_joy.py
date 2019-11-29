6bbe3bdd7bc472430beb76a933556b011d994eed
MickeyKen <mickey333ff@gmail.com>
2019-11-29 11:08:07 +0900

    [Edit] joy stick control node

--- pir2_teleop/scripts/pir2_teleop_joy.py
+++ pir2_teleop/scripts/pir2_teleop_joy.py
@@ -21,7 +21,7 @@ MIN_YAW_POS = -1.3
 LIN_VEL_STEP_SIZE = 0.01
 ANG_VEL_STEP_SIZE = 0.1
 POS_STEP_SIZE = 0.087
-
+YAW_POS_STEP_SIZE = 0.43
 
 class Publishsers():
     def cmd_make(self, x, ang):
@@ -35,6 +35,7 @@ class Publishsers():
     def head_make(self, control_pan_pos, control_tilt_pos, control_yaw_pos):
         head_msg = JointTrajectory()
         jtp_msg = JointTrajectoryPoint()
+        head_msg.joint_names = [ "pan_joint", "tilt_joint", "yaw_joint" ]
         head_msg.header.stamp = rospy.Time.now()
         # jtp_msg.points.positions = [control_pan_pos,control_tilt_pos,control_yaw_pos]
 
@@ -150,6 +151,8 @@ class Subscribe(Publishsers):
             self.control_linear_vel  = 0.0
             self.target_angular_vel  = 0.0
             self.control_angular_vel = 0.0
+
+        if msg.buttons[10] == 1.0:
             self.target_pan_pos = 0.0
             self.control_pan_pos = 0.0
             self.target_tilt_pos = 0.0
@@ -159,9 +162,9 @@ class Subscribe(Publishsers):
 
 
         if msg.axes[4] > 0.0:
-            self.target_tilt_pos = self.checkTiltLimitPosition(self.target_tilt_pos + (msg.axes[4] * POS_STEP_SIZE))
+            self.target_tilt_pos = self.checkTiltLimitPosition(self.target_tilt_pos - (msg.axes[4] * POS_STEP_SIZE))
         elif msg.axes[4] <  0.0:
-            self.target_tilt_pos = self.checkTiltLimitPosition(self.target_tilt_pos + (msg.axes[4] * POS_STEP_SIZE))
+            self.target_tilt_pos = self.checkTiltLimitPosition(self.target_tilt_pos - (msg.axes[4] * POS_STEP_SIZE))
         else:
             pass
 
@@ -172,6 +175,12 @@ class Subscribe(Publishsers):
         else:
             pass
 
+        if msg.buttons[4] == 1.0:
+            self.target_yaw_pos = self.checkYawLimitPosition(self.target_yaw_pos + YAW_POS_STEP_SIZE)
+        elif msg.buttons[5] == 1.0:
+            self.target_yaw_pos = self.checkYawLimitPosition(self.target_yaw_pos - YAW_POS_STEP_SIZE)
+        else:
+            pass
 
         ### display show  ###
         if msg.buttons[0] == 1.0:
@@ -185,6 +194,7 @@ class Subscribe(Publishsers):
         else:
             pass
 
+
         self.image_make(self.image_num)
 
         self.control_linear_vel = self.makeSimpleProfile(self.control_linear_vel, self.target_linear_vel, (LIN_VEL_STEP_SIZE/2.0))
@@ -194,8 +204,9 @@ class Subscribe(Publishsers):
 
         self.control_pan_pos = self.makeSimpleProfile(self.control_pan_pos, self.target_pan_pos, (POS_STEP_SIZE/2.0))
         self.control_tilt_pos = self.makeSimpleProfile(self.control_tilt_pos, self.target_tilt_pos, (POS_STEP_SIZE/2.0))
+        self.control_yaw_pos = self.makeSimpleProfile(self.control_yaw_pos, self.target_yaw_pos, (YAW_POS_STEP_SIZE/2.0))
 
-        self.head_make(self.control_pan_pos, self.control_tilt_pos, 0.0)
+        self.head_make(self.control_pan_pos, self.control_tilt_pos, self.control_yaw_pos)
 
         # print self.vels(self.target_linear_vel, self.target_angular_vel, self.target_pan_pos, self.target_tilt_pos, self.target_yaw_pos)
 
